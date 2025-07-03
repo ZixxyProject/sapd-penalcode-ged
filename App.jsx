e2835286381fde1e84b2bb85299bf49dfce61842
ZixxyProject <brilianozefanya@gmail.com>
2025-07-03 13:50:08 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h1>SAPD Penal Code (GED)</h1>
+      <p>Versi beta aplikasi referensi Penal Code.</p>
+    </div>
+  )
+}
